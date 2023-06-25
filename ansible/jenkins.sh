b0eca7f056c3cbad9075f5466b593eca7dbef338
noobx2empt1L <8340935+noobx7wt2fDL@users.noreply.github.com>
2023-06-25 21:20:39 +0530

    Add files via upload

---
+++ ansible/jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update -y
+apt install jenkins -y
+echo "==========="
+cat /var/lib/jenkins/secrets/initialAdminPassword > /tmp/jenkins-pswd.txt
